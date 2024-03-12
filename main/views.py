305d96639550960dc6ac5f2459c8a18e66a2f4c9
Tatiana Ilves <ilvestania@gmail.com>
2024-03-12 22:26:50 +0200

    Add new app Goods, create models and add fixtures

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+
+    categories = Categories.objects.all()
+
+
     context = {
         'title': 'Fucking shop - Main',
         'content': 'Home page of the Fucking shop',
+        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
