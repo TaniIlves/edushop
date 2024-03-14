78a40817f52b4afa62047baa33ae251ce723f8da
Tatiana Ilves <ilvestania@gmail.com>
2024-03-14 04:07:27 +0200

    Add full text search for Catalog

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     # Packages
     'debug_toolbar',
     # My apps
