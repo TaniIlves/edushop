5a7e3a03fd7502fc0853c798bb6c734444d95471
Tatiana Ilves <ilvestania@gmail.com>
2024-03-15 20:43:40 +0200

    Add templates, forms and models for app Orders

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from carts import views
+from orders import views
 
 app_name = 'orders'
 
--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
         null=True,
         verbose_name='Avatar'
     )
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
 
     class Meta:
         db_table = 'user'
