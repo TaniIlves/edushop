320ba0bfdba39d88d55ac3d3bc12143e571baea1
Tatiana Ilves <ilvestania@gmail.com>
2024-03-15 06:19:15 +0200

    Modify cart remove controller to AJAX

--- carts/views.py
+++ carts/views.py
@@ -23,13 +23,11 @@ def cart_add(request):
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
     user_cart = get_user_carts(request)
-
     cart_items_html = render_to_string(
         'carts/includes/cart_include.html',
         {'carts': user_cart},
         request=request
     )
-
     response_data = {
         'message': 'Item added to cart',
         'cart_items_html': cart_items_html,
@@ -42,8 +40,22 @@ def cart_change(request):
     ...
 
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
+    cart_id = request.POST.get('cart_id')
     cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        'carts/includes/cart_include.html',
+        {'carts': user_cart},
+        request=request
+    )
+    response_data = {
+        'message': 'Item(s) deleted from cart',
+        'cart_items_html': cart_items_html,
+        'quantity_deleted': quantity
+    }
+
+    return JsonResponse(response_data)
