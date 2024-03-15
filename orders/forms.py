76ade9277d55ac98f9d9c6cce38be8f9d06ddd6a
Tatiana Ilves <ilvestania@gmail.com>
2024-03-15 22:56:31 +0200

    Add custom validator for phone number

--- orders/forms.py
+++ orders/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 
@@ -18,6 +20,18 @@ class CreateOrderForm(forms.Form):
             ("1", 'True'),
         ],
     )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError('The phone number must contain only numbers.')
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError('Wrong phone number format')
+
+        return data
 
     # For example:
     # first_name = forms.CharField(
