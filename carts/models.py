aa43a88e929a1a91977d3e93afb585e928664b41
Tatiana Ilves <ilvestania@gmail.com>
2024-03-15 03:32:06 +0200

    Add view for Cart and refactor Cart Templates

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from goods.models import Products
+from users.models import User
+
+
+class CartQueryset(models.QuerySet):
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+
+        return 0
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name='User',
+    )
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name='Good')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Quantity')
+    session_key = models.CharField(max_length=32, blank=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Order added date')
+
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+
+    objects = CartQueryset().as_manager()
+
+    def __str__(self):
+        return f'Cart {self.user.username} | Good {self.product.name} | Quantity {self.quantity}'
+
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+# For future
+# order_id = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name='Order #')
+#
+# class Order(models.Model):
+#     cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE, verbose_name='Cart')
